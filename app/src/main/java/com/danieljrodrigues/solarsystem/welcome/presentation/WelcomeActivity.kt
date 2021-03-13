8e2b0b6aa03f06a05f79c12064278474df8fcc19
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-13 11:08:30 -0300

    Add navigation components

---
+++ app/src/main/java/com/danieljrodrigues/solarsystem/welcome/presentation/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.danieljrodrigues.solarsystem.welcome.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danieljrodrigues.solarsystem.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
