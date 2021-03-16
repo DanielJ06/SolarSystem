547561682045a59de203473d2c9ea8e51b154929
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-03-15 21:39:43 -0300

    From welcome to main activity

--- app/src/main/java/com/danieljrodrigues/solarsystem/welcome/presentation/WelcomeActivity.kt
+++ app/src/main/java/com/danieljrodrigues/solarsystem/welcome/presentation/WelcomeActivity.kt
@@ -1,12 +1,26 @@
 package com.danieljrodrigues.solarsystem.welcome.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.danieljrodrigues.solarsystem.R
+import com.danieljrodrigues.solarsystem.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startNow.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
